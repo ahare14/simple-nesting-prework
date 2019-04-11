d120f08efd46ea4ba81766e9d7967b1d21e82e03
Alex Hare <alexanderhare14@gmail.com>
2019-04-11 00:07:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,6 +66,10 @@ def adding_matz
 
 	programmer_hash =
  		{
+			  :yukihiro_matsumoto => {
+					:known_for => "Ruby",
+					:languages => ["LISP, C"]
+				}			
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,12 +83,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto]
-		#  = {
-		#  	:known_for => "Ruby",
-		#  	:languages => ["LISP,C"]
-		#  }
-		#
+		 programmer_hash[:yukihiro_matsumoto]= {
+		 	:known_for => "Ruby",
+		 	:languages => ["LISP,C"]
+		 }
+		
 end
 
 def changing_alan
